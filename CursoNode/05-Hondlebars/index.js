1a6ed5abd403cf8eb3fb873119f902483cc3ea71
DanielBenatti <daniellopesbenatti2@gmail.com>
2024-06-13 11:56:19 -0300

    sim

--- CursoNode/05-Hondlebars/index.js
+++ CursoNode/05-Hondlebars/index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
 
 //Config
     //template Engine
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
+
+    //conexão Banco de Dados
+    const sequelize = new Sequelize('test','root','',{
+        host: "localhost",
+        dialect: 'mysql'
+    })
+
+//rotas
+app.get('/cadastro',function(req,res){
+    res.render('formulario')
+})
+
+app.post('/add',function(req,res){
+    res.send('formulario enviado')
+})
 
 app.listen(8081,function(){
     console.log("Servidor rodando na url http://localhost:8081")
